50a8ef07e3506230b16d6d11202597946c5f138f
networker <sg123411@gmail.com>
2017-09-21 16:03:37 +0800

    meeting

---
+++ meeting.php
@@ -0,0 +1,55 @@
+<?php	
+	session_start();
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>國立中央大學服務學習網</title>
+		<link href="css/default.css" rel="stylesheet" type="text/css" />
+		<link href="css/menuTheme.css" rel="stylesheet" type="text/css" />
+		<script src="js/JSCookMenu.js" type="text/javascript" ></script>
+		<script src="js/effect.js" type="text/javascript" ></script>
+		<script src="js/theme.js" type="text/javascript" ></script>
+		<script src="js/jquery-1.2.3.min.js" type="text/javascript"></script>
+		<script src="js/jquery.dimensions.min.js" type="text/javascript"></script>		
+		<script src="js/jquery.inputHintBox.js" type="text/javascript"></script>
+		<script src="js/checkForm.js" type="text/javascript"></script>
+		<script src="fckeditor/fckeditor.js" type="text/javascript"></script>
+		<link href="css/bg.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<? require_once("logo.php");?>
+		<? require_once("menu.php");?>
+
+<!-- Content -->
+		<div id="content">
+			<? require_once("sidebar.php");?>
+
+<!-- Publish -->
+			<div id="main">
+				
+
+
+<h3>服務學習會議紀錄</h3>
+
+<ol>
+	
+  <li><a href="" >106學年度</a></li>
+  
+</ol>
+
+
+
+
+
+			</div>
+		</div>
+		
+		<? require_once("footer.php");?>
+
+	</body>
+</html>
+
+
